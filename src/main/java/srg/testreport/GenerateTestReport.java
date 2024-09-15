2aa1bdaa71cbd921c60ff3044b19ef43fd52c3e0
soumya-ranjan-000 <rakiraja7751841655@gmail.com>
2024-09-15 22:22:45 +0530

    updated before test methods

--- src/main/java/srg/testreport/GenerateTestReport.java
+++ src/main/java/srg/testreport/GenerateTestReport.java
@@ -1,7 +1,22 @@
 package srg.testreport;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class GenerateTestReport {
     public static void main(String[] args) {
-
+        Path folderPath = Paths.get("./extent-reports");
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(folderPath)) {
+            // Iterate over the files and folders in the directory
+            for (Path file : stream) {
+                if (!Files.isDirectory(file)) {
+                    System.out.println("File: " + file.getFileName());
+                } else {
+                    System.out.println("Directory: " + file.getFileName());
+                }
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            System.err.println("Error accessing directory: " + e.getMessage());
+        }
     }
 }

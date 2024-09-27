561fbca41ed4be2b3fb5d58606ba18a8c2685a9a
soumya-ranjan-000 <rakiraja7751841655@gmail.com>
2024-09-27 20:41:48 +0530

    updated ExtentManager

--- src/main/java/srg/extentreports/ExtentManager.java
+++ src/main/java/srg/extentreports/ExtentManager.java
@@ -2,7 +2,7 @@ package srg.extentreports;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import srg.util.ResourceHandler;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import java.io.IOException;
 
@@ -11,7 +11,8 @@ public class ExtentManager {
     public synchronized static ExtentReports createExtentReports(String featureFileName) throws IOException {
         ExtentReports extentReports = new ExtentReports();
         ExtentSparkReporter reporter = new ExtentSparkReporter("./extent-reports/"+featureFileName+".html");
-        reporter.loadXMLConfig(ResourceHandler.getFileFromResourcesFolder("spark-config.xml"));
+//        reporter.loadXMLConfig(ResourceHandler.getFileFromResourcesFolder("spark-config.xml"));
+        reporter.config().setTheme(Theme.DARK);
         reporter.config().setReportName(featureFileName);
         extentReports.attachReporter(reporter);
         extentReports.setSystemInfo("Author", "Soumya Ranjan Ghadei");
